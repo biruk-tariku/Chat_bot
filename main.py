9dabcc97376c686d658b9ea98e50fad00c519f2d
Biruk Tariku <128093428+biruk-tariku@users.noreply.github.com>
2023-11-10 08:30:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import re
 import long_responses as long
 
+
 def message_probability(user_message, recognised_words, single_response=False, required_words=[]):
     messagecertainty = 0
     has_required_words = True
